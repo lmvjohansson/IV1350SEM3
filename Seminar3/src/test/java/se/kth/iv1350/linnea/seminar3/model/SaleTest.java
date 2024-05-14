ea11d1126d90e8df7de65e179a9845042acc73d4
lmvjohansson <168458655+lmvjohansson@users.noreply.github.com>
2024-05-14 13:32:47 +0200

    Add files via upload

--- Seminar3/src/test/java/se/kth/iv1350/linnea/seminar3/model/SaleTest.java
+++ Seminar3/src/test/java/se/kth/iv1350/linnea/seminar3/model/SaleTest.java
@@ -11,18 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SaleTest {
     private Sale sale;
     private ItemDTO item;
+    private ItemDTO secondItem;
     
     @BeforeEach
     public void setUp() {
         sale = new Sale();
         item = new ItemDTO("BigWheel Oatmeal", "abc123", new Amount(2821), "BigWheel Oatmeal 500 g, "
                     + "whole grain oats, high fiber, gluten free", 6);
+        secondItem = new ItemDTO("YouGoGo Blueberry", "def456", new Amount(1406), "YouGoGo Blueberry 240 g, "
+                    + "low sugar youghurt, blueberry flavour", 6);
     }
     
     @AfterEach
     public void tearDown() {
         sale = null;
         item = null;
+        secondItem = null;
     }
 
     @Test
@@ -70,5 +74,15 @@ public class SaleTest {
         assertEquals(expectedRunningVat, resultRunningVat, "Register item not calculating running VAT correctly.");
     }
             
-    
+    @Test
+    public void testGetItemListDTO() {
+        sale.registerItem(item);
+        sale.registerItem(item);
+        sale.registerItem(secondItem);
+        ArrayList<LineItemDTO> copiedList = sale.getItemListDTO();
+        assertEquals(copiedList.get(0).getItem(), sale.getItemList().get(0).getItem(), "Copied list contains unexpected ItemDTO");
+        assertEquals(copiedList.get(1).getItem(), sale.getItemList().get(1).getItem(), "Copied list contains unexpected ItemDTO");
+        assertEquals(copiedList.get(0).getQuantity(), sale.getItemList().get(0).getQuantity(), "Copied list contains wrong quantity");
+        assertEquals(copiedList.get(1).getQuantity(), sale.getItemList().get(1).getQuantity(), "Copied list contains wrong quantity");
+    }
 }
